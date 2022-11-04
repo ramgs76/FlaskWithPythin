d04c434531c347473fcabc04bd83afceede0d371
Ram <rammgs76@yahoo.com>
2022-11-04 11:07:14 -0400

    Adding login page

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app=Flask(__name__)
 
 @app.route("/")
 def home():
+    return render_template("home.html")
 
-    names={'Ram':'USA','Singh':'India'}
-    return render_template("index.html",user=names)
+@app.route("/login" ,methods=["GET","POST"])
+def login():
+    return render_template("login.html")    
 
-if __name__=='__main__':
-    app.run(debug=True,host='localhost',port=3000)
-
-
+if __name__ == "__main__":
+    app.run(debug=True ,host="localhost",port=3000)    
