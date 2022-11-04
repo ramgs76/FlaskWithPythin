8da5bf8e67cfae14ce195ad2327939957903efc8
Ram <rammgs76@yahoo.com>
2022-11-03 23:16:11 -0400

    Adding loops

--- app.py
+++ app.py
@@ -1,20 +1,14 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+
+app=Flask(__name__)
+
 @app.route("/")
 def home():
-    return "Hello You are in Ram's World!";
 
-@app.route("/education")
-def learn():
-    return "I am learning web development with Python and Flask"; 
+    names={'Ram':'USA','Singh':'India'}
+    return render_template("index.html",user=names)
 
-@app.route("/<name>")
-def subject(name):
-    return f"I am learning {name}";   
+if __name__=='__main__':
+    app.run(debug=True,host='localhost',port=3000)
 
-@app.route("/square/<int:number>")
-def show_square(number):
-    return f"Square of {number}  is: " + str(number*number);      
 
-if __name__ == "__main__":
-    app.run(debug = True, host = "0.0.0.0", port = 3000)
